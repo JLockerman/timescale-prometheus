6f2016aaa2f41aabd0a184904303581ea55d6854
Joshua Lockerman <josh@timescale.com>
2020-05-27 12:29:52 -0400

    EXPERIMENT: is the pool keeping things alive?

--- pkg/pgmodel/insert_ctx.go
+++ pkg/pgmodel/insert_ctx.go
@@ -1,16 +1,14 @@
 package pgmodel
 
 import (
-	"sync"
-
 	"github.com/timescale/timescale-prometheus/pkg/prompb"
 )
 
-var pool = sync.Pool{
-	New: func() interface{} {
-		return new(InsertCtx)
-	},
-}
+// var pool = sync.Pool{
+// 	New: func() interface{} {
+// 		return new(InsertCtx)
+// 	},
+// }
 
 type InsertCtx struct {
 	WriteRequest prompb.WriteRequest
@@ -18,32 +16,35 @@ type InsertCtx struct {
 }
 
 func NewInsertCtx() *InsertCtx {
-	return pool.Get().(*InsertCtx)
+	// return pool.Get().(*InsertCtx)
+	return new(InsertCtx)
 }
 
 func (t *InsertCtx) clear() {
 	t.ClearTimeSeries()
-	for i := range t.Labels {
-		t.Labels[i].reset()
-	}
-	t.Labels = t.Labels[:0]
+	// for i := range t.Labels {
+	// 	t.Labels[i].reset()
+	// }
+	// t.Labels = t.Labels[:0]
+	t.Labels = nil
 }
 
 func (t *InsertCtx) ClearTimeSeries() {
-	for i := range t.WriteRequest.Timeseries {
-		ts := &t.WriteRequest.Timeseries[i]
-		for j := range ts.Labels {
-			ts.Labels[j] = prompb.Label{}
-		}
-		// TODO This won't be needed once Sample is a value type
-		for j := range ts.Samples {
-			ts.Samples[j] = prompb.Sample{}
-		}
-		ts.Labels = ts.Labels[:0]
-		ts.Samples = ts.Samples[:0]
-		ts.XXX_unrecognized = nil
-	}
-	t.WriteRequest.Timeseries = t.WriteRequest.Timeseries[:0]
+	// for i := range t.WriteRequest.Timeseries {
+	// 	ts := &t.WriteRequest.Timeseries[i]
+	// 	for j := range ts.Labels {
+	// 		ts.Labels[j] = prompb.Label{}
+	// 	}
+	// 	// TODO This won't be needed once Sample is a value type
+	// 	for j := range ts.Samples {
+	// 		ts.Samples[j] = prompb.Sample{}
+	// 	}
+	// 	ts.Labels = ts.Labels[:0]
+	// 	ts.Samples = ts.Samples[:0]
+	// 	ts.XXX_unrecognized = nil
+	// }
+	// t.WriteRequest.Timeseries = t.WriteRequest.Timeseries[:0]
+	t.WriteRequest = prompb.WriteRequest{}
 }
 
 func (t *InsertCtx) NewLabels(length int) *Labels {
@@ -68,5 +69,5 @@ func (t *InsertCtx) NewLabels(length int) *Labels {
 
 func (t *InsertCtx) Close() {
 	t.clear()
-	pool.Put(t)
+	// pool.Put(t)
 }
